e216608277e8b8e595d1673fe26f0ebe564ae71d
GodKra <godkra567@gmail.com>
2025-03-26 20:32:24 +0800

    add first time sign up

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService extends ChangeNotifier {
+  String? uid;
   bool _isAuthenticated = false;
 
   AuthService() {
@@ -13,11 +15,20 @@ class AuthService extends ChangeNotifier {
 
   void _authStateListener() {
     FirebaseAuth.instance.authStateChanges().listen((User? user) async {
+      uid = user?.uid;
       _isAuthenticated = user != null;
       notifyListeners();
     });
   }
 
+  Future<void> _storeUserData(User user) async {
+    await FirebaseFirestore.instance.collection("users").doc(user.uid).set({
+      "name": user.displayName,
+      "email": user.email,
+      "auth_provider": "google.com" // for now
+    });
+  }
+    
   Future<UserCredential?> login() async {
     try {
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
@@ -34,9 +45,14 @@ class AuthService extends ChangeNotifier {
         idToken: googleAuth.idToken,
       );
       
-      final userCred =  await FirebaseAuth.instance.signInWithCredential(credential);
+      final userCred = await FirebaseAuth.instance.signInWithCredential(credential);
 
       notifyListeners();
+
+      // First time sign up.
+      if (userCred.additionalUserInfo?.isNewUser ?? false) {
+        await _storeUserData(userCred.user!);
+      }
 
       return userCred;
     } catch (e) {
